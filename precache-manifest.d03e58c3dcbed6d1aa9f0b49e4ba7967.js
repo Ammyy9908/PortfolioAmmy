46bda6cfbd0e37c05892513fe03b744f20a9f3e0
Ammyy9908 <sb78639@gmail.com>
2020-03-07 13:17:31 +0530

    Updates

---
+++ precache-manifest.d03e58c3dcbed6d1aa9f0b49e4ba7967.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47ed65e4973c4e0f06522d94d620d123",
+    "url": "/PortfolioAmmy/index.html"
+  },
+  {
+    "revision": "0f4d6c823240e55c61ff",
+    "url": "/PortfolioAmmy/static/js/2.7245b878.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/PortfolioAmmy/static/js/2.7245b878.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0baa08d8f69acd9477ab",
+    "url": "/PortfolioAmmy/static/js/main.5903662f.chunk.js"
+  },
+  {
+    "revision": "ca1c1b0180bd5daf97c5",
+    "url": "/PortfolioAmmy/static/js/runtime-main.ca8c0146.js"
+  }
+]);
